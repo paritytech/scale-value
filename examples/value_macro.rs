3e233343a6f0c46eeb0d8d43b08caaacb04e9cf0
Tadeo hepperle <tadeo@do-mix.de>
2023-06-29 19:56:22 +0200

    clippy

--- examples/value_macro.rs
+++ examples/value_macro.rs
@@ -1,8 +1,8 @@
-use scale_value;
+use scale_value::value;
 
 fn main() {
     // The `value` macro can construct Value's like so:
-    let val = scale_value::value!({
+    let val = value!({
         hello: "Hello",
         world: 12345,
         variant: Variant {
